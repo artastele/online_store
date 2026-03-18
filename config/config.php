ed4dc350abf26a6a2b8b243fe45320d64cc3bfe9
artastele <allysacanonizado43@gmail.com>
2026-03-18 23:39:50 +0800

    PayMongo Integration

--- config/config.php
+++ config/config.php
@@ -13,4 +13,4 @@ define('DB_NAME', 'online_store');
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) die("DB Connection failed: " . $conn->connect_error);
 
-define('PAYMONGO_SECRET', 'sk_test_fvqFFfJt9r2GFiGYVtH46bqe');
+define('PAYMONGO_SECRET', 'your_paymongo_secret_key');
